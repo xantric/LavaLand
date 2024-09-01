c0aa11bdcac9800ff957f430dd0684258839212a
Beginner10617 <wasihusain23@iitk.ac.in>
2024-09-01 19:30:23 +0530

    aiming

--- Assets/Scripts/Shoots.cs
+++ Assets/Scripts/Shoots.cs
@@ -4,19 +4,31 @@ using UnityEngine;
 
 public class Shoots : MonoBehaviour
 {
-    public Transform arCamera;
-    public GameObject projectile;
-
-    public float shootForce = 700.0f;
+    public Transform Gun;
+    public float range;
+    public GameObject crossHair;
+    RaycastHit hit;
+//    public GameObject projectile;
+//    public float shootForce = 700.0f;
 
     // Update is called once per frame
     void Update()
     {
+        crossHair.transform.position = hit.point;
+        crossHair.transform.forward = -Gun.transform.forward;
+
         if(Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began)
         {
-            GameObject bullet = Instantiate(projectile, arCamera.position, arCamera.rotation) as GameObject;
-            bullet.GetComponent<Rigidbody>().AddForce(arCamera.forward*shootForce);
+            Shoot();
         }
         
+    }
+
+    void Shoot()
+    {
+        if(Physics.Raycast(Gun.position, Gun.forward, out hit, range))
+        {
+            crossHair.transform.position = hit.transform.position;
+        }
     }
 }
