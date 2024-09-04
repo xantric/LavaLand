d1031656317d93b54ceb3d1d6b10625cfa4ea5df
Beginner10617 <wasihusain23@iitk.ac.in>
2024-09-04 12:42:56 +0530

    UI

--- Assets/Scripts/Health System.cs
+++ Assets/Scripts/Health System.cs
@@ -1,23 +1,46 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class HealthSystem : MonoBehaviour
 {
     float health;
-    public float damage = 0;
+    float t = 0;
+    public float damage = 20;
+    float length, width;
+    public GameObject healthBar;
     // Start is called before the first frame update
     void Start()
     {
+        t = 0 ;
+        length = healthBar.GetComponent<Image>().rectTransform.rect.width / 100;
+        width = 25;
         health = 100;
+        damage = 20;
     }
 
     void OnCollisionEnter(Collision other)
     {
-        health -= damage;
+        if(other.gameObject.CompareTag("Enemy"))
+        {
+            health -= damage;
+            Destroy(other.gameObject);
+        }
         if(health <= 0)
         {
-            //GameOver
+            //game lost
+        }
+
+    }
+    void Update()
+    {
+        t += Time.deltaTime;
+
+        healthBar.GetComponent<Image>().rectTransform.sizeDelta = new Vector2(length*health, width) ;
+        if(t > 120)
+        {
+            //game won
         }
     }
 }
---
+++ Assets/Scripts/UI.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UI : MonoBehaviour
+{
+    public GameObject pauseWindow;
+    void Start()
+    {
+        pauseWindow.SetActive(false);
+    }
+    public void Pause()
+    {
+        pauseWindow.SetActive(true);
+        Time.timeScale = 0;
+    }
+    public void Resume()
+    {
+        Time.timeScale = 1;
+        pauseWindow.SetActive(false);
+        
+    }
+    public void ExitPause()
+    {
+        int curr = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(curr - 1);
+    }
+    public void Play()
+    {
+        int curr = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(curr + 1);
+    }
+    public void Exit()
+    {
+        Application.Quit();
+    }
+}
